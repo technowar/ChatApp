2adfd55cb7f8ea76424bfb5718ee2c53c06eb4c2
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2015-03-01 13:42:49 +0000

    Refactor

--- index.js
+++ index.js
@@ -1,49 +1,22 @@
 'use strict';
 
-var Readline = require('readline');
+var Chat = require('./lib/chat');
+
 var Io = require('socket.io-client')('http://localhost:3030');
 
-var rl = Readline.createInterface({
-	'input' : process.stdin,
-	'output' : process.stdout
-});
-
-var nick;
-
-rl.question('Set Nickname: ', function (data) {
-	nick = data;
-
-	Io.emit('send', {
-		'type' : 'notice',
-		'message' : nick + ' has joined the flow.'
-	});
-
-	rl.prompt(true);
-});
-
-rl.on('line', function (data) {
-	Io.emit('send', {
-		'type' : 'chat',
-		'message' : data,
-		'nick' : nick
-	});
-
-	rl.prompt(true);
-});
+Chat.options.setNickname();
+Chat.options.setMessages();
 
 Io.on('message', function (data) {
 	if (data.type === 'notice') {
-		return displayMsg(data.message);
+		return Chat.options.relayMessages(data.message);
 	}
 
-	return displayMsg('<' + data.nick + '> ' + data.message);
+	return Chat.options.relayMessages('<' + data.nick + '> ' + data.message);
 });
 
-function displayMsg (msg) {
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-
-	console.log(msg);
-
-	rl.prompt(true);
-}
+exports.options = {
+	'emitMessages' : function (userOptions) {
+		Io.emit('send', userOptions);
+	}
+};
