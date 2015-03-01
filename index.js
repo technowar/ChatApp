9dc522f76ddbe04748289a8a647962fc690ed7ec
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2015-03-01 09:03:29 +0000

    Implement chat

--- index.js
+++ index.js
@@ -8,11 +8,42 @@ var rl = Readline.createInterface({
 	'output' : process.stdout
 });
 
-rl.question('Set Nickname: ', function (nick) {
+var nick;
+
+rl.question('Set Nickname: ', function (data) {
+	nick = data;
+
 	Io.emit('send', {
 		'type' : 'notice',
-		'message' : nick + ' has joined the flow'
+		'message' : nick + ' has joined the flow.'
 	});
 
 	rl.prompt(true);
 });
+
+rl.on('line', function (data) {
+	Io.emit('send', {
+		'type' : 'chat',
+		'message' : data,
+		'nick' : nick
+	});
+
+	rl.prompt(true);
+});
+
+Io.on('message', function (data) {
+	if (data.type === 'notice') {
+		return displayMsg(data.message);
+	}
+
+	return displayMsg('<' + data.nick + '> ' + data.message);
+});
+
+function displayMsg (msg) {
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+
+	console.log(msg);
+
+	rl.prompt(true);
+}
